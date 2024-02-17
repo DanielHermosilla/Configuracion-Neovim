db0b000da7c46e19e26567b23ae30e8026333c1a
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2024-02-17 00:05:29 -0300

    Se añade LSP y configuración para los warnings (code actions)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,35 @@
 return {
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      
-      keys = { 
-	  { -- Buscar archivos 
-	    "<leader>f",
-	    function()
-	      require('telescope.builtin').find_files()
-	    end,
-	    desc = "Telescope Find Files",
-	  },
-	  { -- Buscar dentro de los buffers 
-	    "<leader>b",
-	    function()
-	      require("telescope.builtin").buffers()
-	    end,
-	    desc = "Telescope buffers"
-	  }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+	{ -- Buscar archivos 
+	  "<leader>f",
+	  function()
+	    require('telescope.builtin').find_files()
+	  end,
+	  desc = "Telescope Find Files",
 	},
-      }
+	{ -- Buscar dentro de los buffers 
+	  "<leader>b",
+	  function()
+	    require("telescope.builtin").buffers()
+	  end,
+	  desc = "Telescope buffers"
+	}
+      },
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",		-- Permite ver errores con Telescope
+    config = function()
+      require("telescope").setup({
+	extensions = {
+	  ["ui-select"] = {
+	    require("telescope.themes").get_dropdown {
+	    }
+	  }
+	}
+      })
+    end
+  },
+ }
