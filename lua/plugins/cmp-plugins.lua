45fac03d757cf15d15f7f956ce1db6ed47477c98
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2024-02-17 13:49:39 -0300

    CMP y snippets

--- lua/plugins/cmp-plugins.lua
+++ lua/plugins/cmp-plugins.lua
@@ -1,16 +1,75 @@
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-buffer", -- Autocompletado de buffers
-		"hrsh7th/cmp-path", -- Autocompletado de paths
+	{
 		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-cmdline", -- Autocompletado para la línea de comandos
-		"hrsh7th/cmp-git", -- Autocompletado para git
-		"L3MON4D3/LuaSnip", -- Snippets de Lua
-		"saadparwaiz1/cmp_luasnip", -- Snippets de Lua
-		"windwp/nvim-autopairs",
 	},
-	event = "VeryLazy",
-	main = "config.plugins.cmp",
-	config = false,
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local snip_status_ok, luasnip = pcall(require, "luasnip")
+			if not snip_status_ok then
+				return
+			end
+
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }), -- Mover la ventana hacia abajo
+					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }), -- Mover la ventana hacia arriba
+					["<C-Space>"] = cmp.mapping.complete(),              -- Abre todo el dialogo de texto
+					["<C-e>"] = cmp.mapping({
+						i = cmp.mapping.abort(),
+						c = cmp.mapping.close(),
+					}),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(function(fallback) -- Permite navegar con Tab
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback) -- Permite navegar con Tab
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
+
