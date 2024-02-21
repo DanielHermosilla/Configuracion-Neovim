e61f6d1e449681e8ee5f9a2b4f988d6376602929
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2024-02-21 00:08:43 -0300

    Optimización de CMP y adición de más fuentes

--- lua/plugins/cmp-plugins.lua
+++ lua/plugins/cmp-plugins.lua
@@ -3,20 +3,46 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	{
-		"micangl/cmp-vimtex", -- Snippets que funcionan con Vimtex
-	},
-	{
-		"SirVer/ultisnips"
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
 		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-path" }, -- Motor de compleción para los path
+			{ "hrsh7th/cmp-buffer" }, -- Motor de compleción para los buffer
+			{ "hrsh7th/cmp-cmdline" }, -- Motor de compleción para los CMD
+			{ "hrsh7th/cmp-nvim-lsp-document-symbol" },
+			{ "kdheepak/cmp-latex-symbols" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{
+				"L3MON4D3/LuaSnip",
+				build = vim.fn.has("win32") ~= 0 and "make install_jsregexp" or nil, -- Caso que se corre en Windows
+				dependencies = {
+					"rafamadriz/friendly-snippets",
+					"saadparwaiz1/cmp_luasnip"
+				},
+				config = function(_, opts)
+					if opts then
+						require("luasnip").config.setup(opts)
+					end
+					vim.tbl_map(function(type)
+						require("luasnip.loaders.from_" .. type).lazy_load()
+					end, { "vscode", "snipmate" })
+					require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/Snippets/" }) -- friendly-snippets - enable standardized comments snippets
+					require("luasnip").filetype_extend("typescript", { "tsdoc" })
+					require("luasnip").filetype_extend("javascript", { "jsdoc" })
+					require("luasnip").filetype_extend("lua", { "luadoc" })
+					require("luasnip").filetype_extend("python", { "pydoc" })
+					require("luasnip").filetype_extend("rust", { "rustdoc" })
+					require("luasnip").filetype_extend("cs", { "csharpdoc" })
+					require("luasnip").filetype_extend("java", { "javadoc" })
+					require("luasnip").filetype_extend("c", { "cdoc" })
+					require("luasnip").filetype_extend("cpp", { "cppdoc" })
+					require("luasnip").filetype_extend("php", { "phpdoc" })
+					require("luasnip").filetype_extend("kotlin", { "kdoc" })
+					require("luasnip").filetype_extend("ruby", { "rdoc" })
+					require("luasnip").filetype_extend("sh", { "shelldoc" })
+				end,
+			}, -- Snippets manager
+
+		},
 		config = function()
 			local snip_status_ok, luasnip = pcall(require, "luasnip")
 			if not snip_status_ok then
