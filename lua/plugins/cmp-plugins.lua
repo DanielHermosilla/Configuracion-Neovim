9a4f8256299c5ffad4dc2d558d80517a407a7328
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2024-03-03 21:08:13 -0300

    Se añade super-tab

--- lua/plugins/cmp-plugins.lua
+++ lua/plugins/cmp-plugins.lua
@@ -18,7 +18,8 @@ return {
 					"rafamadriz/friendly-snippets",
 					"saadparwaiz1/cmp_luasnip",
 					"ckunte/latex-snippets-vim",
-				},
+					"micangl/cmp-vimtex",
+				}, -- Snippets a utilizar
 				config = function(_, opts)
 					if opts then
 						require("luasnip").config.setup(opts)
@@ -42,7 +43,6 @@ return {
 					require("luasnip").filetype_extend("sh", { "shelldoc" })
 				end,
 			}, -- Snippets manager
-
 		},
 		config = function()
 			local snip_status_ok, luasnip = pcall(require, "luasnip")
@@ -52,6 +52,12 @@ return {
 
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local has_words_before = function() -- Función para el "super-tab"
+			  unpack = unpack or table.unpack
+			  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
 			cmp.setup({
 				snippet = {
@@ -68,20 +74,41 @@ return {
 					["<C-k>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }), -- Mover la ventana hacia abajo
 					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }), -- Mover la ventana hacia arriba
-					["<C-Space>"] = cmp.mapping.complete(),              -- Abre todo el dialogo de texto
+					["<C-Space>"] = cmp.mapping.complete(), -- Abre todo el dialogo de texto
 					["<C-e>"] = cmp.mapping({
 						i = cmp.mapping.abort(),
 						c = cmp.mapping.close(),
 					}),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.select_next_item(), -- Permite la navegación con Tab
-					["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Permite la navegación con Tab
+					["<Tab>"] = cmp.mapping(function(fallback) -- Super-tab, permite seleccionar y navegar
+						if cmp.visible() then
+							cmp.select_next_item()
+							-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+							-- that way you will only jump inside the snippet region
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback) -- Super-tab, permite seleccionar y navegar
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
 					{ name = "path" },
-					--{ name = "cmdline" }, -- Al parecer no funciona bien 
+					--{ name = "cmdline" }, -- Al parecer no funciona bien
 					{ name = "cmp-vimtex" },
 				}, {
 					{ name = "buffer" },
